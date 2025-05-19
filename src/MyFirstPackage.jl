4d74eb1eb6aa0905a65d268e7ab86678747d03dd
Erdong Huang <huangerdonghq@163.com>
2025-05-19 14:29:00 +0800

    initial commit

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -1,22 +1,22 @@
 module MyFirstPackage
 
 using Graphs
-using TropicalNumbers
-using LinearAlgebra
 
 export shortest_distance_matrix
 
-function shortest_distance_matrix(g::Graphs.AbstractGraph)
+function shortest_distance_matrix(g::Graph)
     n = nv(g)
-    A = adjacency_matrix(g)           
-    T = map(x -> x == 1 ? TropicalMinPlus(1) : zero(TropicalMinPlus{Int}), A)
-    T .= T .+ Diagonal(fill(TropicalMinPlus(0), n))
-    Tn = T^n
-    dist = Array{Int}(undef, n, n)
-    for i in 1:n, j in 1:n
-        dist[i, j] = Int(Tn[i, j])
+    dist_matrix = fill(-1, n, n)
+    for i in 1:n
+        sp = dijkstra_shortest_paths(g, i)
+        for j in 1:n
+            d = sp.dists[j]
+            if !isinf(d)
+                dist_matrix[i, j] = trunc(Int, d)
+            end
+        end
     end
-    return dist
+    return dist_matrix
 end
 
 end

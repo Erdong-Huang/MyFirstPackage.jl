ad44f0b1a093c720c61e4b7cce38015d6fe80ede
Erdong Huang <huangerdonghq@163.com>
2025-05-19 14:22:48 +0800

    initial commit

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -1,8 +1,22 @@
 module MyFirstPackage
 
-using TropicalNumbers, LinearAlgebra, Graphs
+using Graphs
+using TropicalNumbers
+using LinearAlgebra
 
-export cal_path
-include("shortestpath.jl")
+export shortest_distance_matrix
+
+function shortest_distance_matrix(g::Graphs.AbstractGraph)
+    n = nv(g)
+    A = adjacency_matrix(g)           
+    T = map(x -> x == 1 ? TropicalMinPlus(1) : zero(TropicalMinPlus{Int}), A)
+    T .= T .+ Diagonal(fill(TropicalMinPlus(0), n))
+    Tn = T^n
+    dist = Array{Int}(undef, n, n)
+    for i in 1:n, j in 1:n
+        dist[i, j] = Int(Tn[i, j])
+    end
+    return dist
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,17 @@
-using MyFirstPackage
 using Test
+using Graphs
+using MyFirstPackage
 
-using TropicalNumbers, LinearAlgebra, Graphs
+@testset "shortest-path" begin
+    g1 = smallgraph(:petersen)
+    D1 = shortest_distance_matrix(g1)
+    @test size(D1) == (nv(g1), nv(g1))
+    for i in 1:nv(g1), j in 1:nv(g1)
+        sp = dijkstra_shortest_paths(g1, i)
+        @test D1[i, j] == sp.dists[j]
+    end
 
-@testset "MyFirstPackage.jl" begin
-    include("shortestpath.jl")
+    g2 = path_graph(5)
+    D2 = shortest_distance_matrix(g2)
+    @test D2 == [abs(i-j) for i in 1:5, j in 1:5]
 end
